f61e73e81699007a9627a183f49445ad915c8a94
“Ievgen” <baranoffei@gmail.com>
2019-03-16 21:37:18 +0200

    MVP - Add AUTH_REDUCER - first project reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,11 +6,34 @@ import {
   MetaReducer
 } from "@ngrx/store";
 import { environment } from "../../environments/environment";
+import { User } from "../auth/models/user.model";
+import { AuthActionTypes } from "../auth/auth.actions";
 
-// tslint:disable-next-line:no-empty-interface
-export interface AppState {}
+// tslint:disable-next-line:interface-over-type-literal
+type AuthState = {
+  loggedIn: boolean;
+  user: User;
+};
 
-export const reducers: ActionReducerMap<AppState> = {};
+export interface AppState {
+  auth: AuthState;
+}
+
+function authReducer(state: AuthState, action): AuthState {
+  switch (action.type) {
+    case AuthActionTypes.LoginAction:
+      return {
+        loggedIn: true,
+        user: action.payload.user
+      };
+    default:
+      return state;
+  }
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+  auth: authReducer
+};
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
   ? []
